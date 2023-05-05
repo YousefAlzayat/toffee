15e232568a0aedc0928a6118720ca55661fdc7bc
Aya Osama <101758186+oaya2411@users.noreply.github.com>
2023-05-06 01:51:59 +0300

    Update Item.java
    Item class without throwing exception

--- Item.java
+++ Item.java
@@ -1,30 +1,26 @@
 import jdk.jfr.Category;
 import java.util.Objects;
 import java.util.Scanner;
-import static java.lang.System.exit;//to use exit(0) in switch to end switch statement
 public class Item {
     private String name;
     private String description = "";
     private int price = 0;
     private int availableAmount;
     private String brand;
-    private int discountAmount;
+    private String discountAmount;
     private String categoryName;
     public static  int increaseAmountSold = 0;
-    private Admin admin;
-
     public Item() {
         this.name = "";
         this.price = 0;
         this.description = "";
         this.availableAmount = 0;
         this.brand = "";
-        this.discountAmount = 0;
+        this.discountAmount = "";
         this.categoryName = "";
-        this.admin = null;
     }
 
-    public Item(String name, int price, String des, int amount, String bName, int discount, String CName, Admin a) {
+    public Item(String name, int price, String des, int amount, String bName, String discount, String CName, Admin a) {
         this.name = name;
         this.price = price;
         this.description = des;
@@ -32,56 +28,42 @@ public class Item {
         this.brand = bName;
         this.discountAmount = discount;
         this.categoryName = CName;
-        this.admin = a;
-    }
-
-    public void setAdmin(Admin admin) {
-        this.admin = admin;
-    }
-
-    public Admin getAdmin() {
-        return admin;
     }
 
     private String IconPath;
     Scanner in = new Scanner(System.in);
-    public void setBrandIcon(Admin admin){
-        System.out.println("Please enter the icon path: ");
+    public void setBrandIcon(){
+        System.out.print("Please enter the icon path: ");
         this.IconPath = in.next();
     }
-    public void setName(Admin a){
-        System.out.println("please enter the name of Item");
+    public void setName(){
+        System.out.print("please enter the name of Item: ");
         this.name = in.next();
+        in.nextLine();
     }
     public String getName(){
         return name;
     }
-    public void setDescription(Admin admin){
+    public void setDescription(){
         System.out.println("Please enter the description: ");
-        System.out.println("-enter '\n' to end entering process-");
-        String n;
-        n = in.next();
-        while(!Objects.equals(n, "\n")){
-            this.description += n;
-        }
-
+        this.description = in.nextLine();
     }
     public String getDescription() {
         return description;
     }
-    public void setPrice(Admin a){
-        System.out.println("Please enter price: ");
+    public void setPrice(){
+        System.out.print("Please enter price: ");
         this.price = in.nextInt();
     }
     public int getPrice(){
-        return price;
+        return this.price;
     }
-    public void setAvailableAmount(Admin a){
-        System.out.println("Please enter the available amount: ");
+    public void setAvailableAmount(){
+        System.out.print("Please enter the available amount: ");
         this.availableAmount = in.nextInt();
     }
     public int getAvailableAmount(){
-        return availableAmount;
+        return this.availableAmount;
     }
     public boolean bestSeller(){
         if(increaseAmountSold > 100){
@@ -89,59 +71,31 @@ public class Item {
         }
         return false;
     }
-    public void setBrandName(Admin a){
-        System.out.println("please enter the brand name: ");
+    public void setBrandName(){
+        System.out.print("please enter the brand name: ");
         this.brand = in.next();
     }
-    public String getBrandName(){
-        return brand;
+    public String getBrandName() {
+        return this.brand;
     }
-    public void setDiscountAmount(Admin a){
-        System.out.println("Please enter discount amount: ");
-        this.discountAmount = in.nextInt();
+    public void setDiscountAmount(){
+        System.out.print("Please enter discount amount: ");
+        this.discountAmount = in.nextLine();
+        in.nextLine();
     }
-    public int getDiscountAmount(){
+    public String getDiscountAmount(){
         return discountAmount;
     }
-    public void setCategoryName(Admin a){
-        System.out.println("Please enter category name: ");
-        this.categoryName = in.next();
+    public void setCategoryName(String CName){
+        this.categoryName = CName;
     }
     public String getCategoryName(){
         return categoryName;
     }
-    public void updateItemInfo(Admin a) {
-        int choice = 0;
-        do {
-            System.out.println("Properties of the item: \n" +
-                    "1.Name.\n" +
-                    "2.Description.\n" +
-                    "3.Price.\n" +
-                    "4.Available Amount.\n" +
-                    "5.Category Name.\n" +
-                    "6.Brand Name.\n" +
-                    "7.Brand Icon Path.\n" +
-                    "8.Exit.");
-            System.out.println("Please enter number of property you want to update: ");
-            choice = in.nextInt();
-            switch (choice) {
-                default -> System.out.println("Invalid choice, Try again.");
-                case 1 ->
-                    setName(a);
-                case 2 -> setDescription(a);
-                case 3 -> setPrice(a);
-                case 4 -> setAvailableAmount(a);
-                case 5 -> setCategoryName(a);
-                case 6 -> setBrandName(a);
-                case 7 -> setBrandIcon(a);
-                case 8 -> {
-                    System.out.println("EXIT...");
-                    exit(0);
-                }
-            }
-
-        }while (true);
-    }
-
-
+     public boolean AvailableORNot(){
+         if(availableAmount == 0){
+             return false;
+         }
+         return true;
+     }
 }
