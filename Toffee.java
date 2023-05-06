6b614b56d31a176ab94b18888bc94ed7348c882a
Aya Osama <101758186+oaya2411@users.noreply.github.com>
2023-05-07 00:50:15 +0300

    Update Toffee.java

--- Toffee.java
+++ Toffee.java
@@ -1,65 +1,138 @@
-import java.util.ArrayList;
+import java.util.Objects;
+import java.util.Vector;
 import java.util.Scanner;
 
+import static java.lang.System.exit;
+import static java.lang.System.in;
+
 public class Toffee {
-    
-    private ArrayList<Account> accounts;
-    private ArrayList<Category> categories;
+
+    private  Vector<Account> accounts = new Vector<>();
+    private  Vector<Category> categories = new Vector<>();
     private String tempOTP = "";
-
-    public void displaySystem(){
-        Scanner reader = new Scanner(System.in);
-        System.out.println("-=-=-=-=-=-=-=-=-");
-        System.out.println("Welcome to Toffee!");
-        System.out.println("Please enter what you would like to do from the following: ");
-        System.out.println("(1) Register");
-        System.out.println("(2) Login");
-        System.out.println("(3) View Catalogue");
-        System.out.println("(4) Exit");
-        System.out.print("Choice: ");
-        int choice = reader.nextInt();
-
-        System.out.print("\033[H\033[2J"); 
-        switch(choice){
-            case 1:
-                System.out.println("You chose the register option");
-                break;
-
-            case 2:
-                System.out.println("You chose the LogIn option");
-                break;
-                
-            case 3:
-                displayCategories();
-                break;
-
-                default:
-                System.out.println("Thank you for using Toffee, have a nice day!");
-                System.out.println("Exiting...");
-                break;
-        }
-
-        reader.close();
-
-
+    public Toffee(Vector<Account> accounts, Vector<Category> categories) {
+        this.accounts = accounts;
+        this.categories = categories;
     }
 
+    public Toffee(){}
     public void displayCategories(){
-
+        System.out.print("{ ");
+        for(int i = 0; i < categories.size(); i++){
+            System.out.print(categories.get(i) + " ");
+        }
+        System.out.println("}");
     }
 
+    public int foundedMail(String mail){
+        for(int i = 0; i < accounts.size(); i++){
+            if(Objects.equals(accounts.get(i).getMail(), mail)){
+                return i;
+            }
+        }
+        return -1;
+    }
+
+    public String correctPassword(String pass){
+        for(int i = 0; i < accounts.size(); i++){
+            if(Objects.equals(accounts.get(i).getPassword(), pass)){
+                return accounts.get(i).getPassword();
+            }
+        }
+        return "Incorrect Password";
+    }
+
+    public void displaySystem() {
+        Scanner reader = new Scanner(System.in);
+        int choice = 0;
+        do {
+            System.out.println("-=-=-=-=-=-=-=-=-");
+            System.out.println("Welcome to Toffee!");
+            System.out.println("Please enter what you would like to do from the following: ");
+            System.out.println("(1) Register");
+            System.out.println("(2) Login");
+            System.out.println("(3) View Catalogue");
+            System.out.println("(4) Exit");
+            System.out.print("Choice: ");
+            choice = reader.nextInt();
+            System.out.print("\033[H\033[2J");
+            switch (choice) {
+                default -> System.out.println("Invalid Input, Try Again");
+                case 1 -> {
+                    Register r = new Register();
+                    Account a = new Account();
+                    r.createAccount(a);
+                    System.out.println("Successfully Created Account.");
+                    accounts.add(a);
+                    System.out.println(a.getFName() + " " + a.getSName());
+//                    System.out.println(a.getLoyalityPoints());
+                    displayCategories();
+                }
+                case 2 -> {
+                    LogIn l = new LogIn();
+                    int ch = foundedMail(l.enterMail());
+                    while(ch == -1){
+                        System.out.println("InValidMail");
+                        ch = foundedMail(l.enterMail());
+                        if(ch == -1){
+                            System.out.println("Did you want to register instead of logIn?\n1. YES.\n2. NO.");
+                            int d = reader.nextInt();
+                            if(d == 1){
+                                displaySystem();
+                                break;
+                            }
+                        }
+                    }
+                    if(Objects.equals(correctPassword(l.enterPassword()), accounts.get(ch).getPassword())){
+                        System.out.print(accounts.get(ch).getFName() + " " + accounts.get(ch).getSName());
+//                        System.out.println(accounts.get(ch).getLoyalityPoints());
+                        displayCategories();
+                    }else{
+                        while(true){
+                            System.out.println("This is wrong password for this mail");
+                            System.out.print("You can:\n1. Update Password\n2. Enter password again.\nEnter your choice: ");
+                            int n = reader.nextInt();
+                            if(n == 1){
+                               String newPass = l.enterPassword();
+                               if(Objects.equals(correctPassword(newPass), accounts.get(ch).getPassword())){
+                                   System.out.print(accounts.get(ch).getFName() + " " + accounts.get(ch).getSName());
+//                                 System.out.println(accounts.get(ch).getLoyalityPoints());
+                                   displayCategories();
+                                   break;
+                               }
+                            }else if(n == 2){
+                                accounts.get(ch).updatePassword(l.forgetPassword());
+                                System.out.print(accounts.get(ch).getFName() + " " + accounts.get(ch).getSName());
+//                                 System.out.println(accounts.get(ch).getLoyalityPoints());
+                                displayCategories();
+                                break;
+                            }else{
+                                System.out.println("Try Again....");
+                            }
+                        }
+                    }
+                }
+                case 3 -> displayCategories();
+                case 4 -> {
+                    System.out.println("Thank you for using Toffee, have a nice day!");
+                    System.out.println("Exiting...");
+                    exit(0);
+                }
+            }
+        } while (true);
+
+    }
     public String createOTP(){
         Integer otp = (int) Math.floor(Math.random() * (9999 - 1000 + 1) + 1000);
         tempOTP = otp.toString();
 
         return tempOTP;
     }
-
     public boolean correctOTP(String s){
         if(s == tempOTP) {
             tempOTP = "";
             return true;
-        } 
+        }
         return false;
     }
 
@@ -72,3 +145,4 @@ public class Toffee {
     }
 
 }
+
