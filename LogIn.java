1f9de12bd067148ce96c821c2491c334a5a6580e
Aya Osama <101758186+oaya2411@users.noreply.github.com>
2023-05-07 00:48:31 +0300

    Update LogIn.java

--- LogIn.java
+++ LogIn.java
@@ -1,2 +1,43 @@
+import java.util.Scanner;
+import java.util.regex.Pattern;
+
 public class LogIn {
+    LogIn(){};
+    Scanner in = new Scanner(System.in);
+    String m;
+    public String enterMail(){
+        System.out.print("Please Enter Your Mail: ");
+        m = in.next();
+        return m;
+    }
+    public String enterPassword(){
+        System.out.print("Please Enter Your Password: ");
+        return in.next();
+    }
+    public boolean checkPasswordGuidelines(String pass){
+        if(Pattern.matches("(?=.*[a-zA-z])(?=.*[0-9])(?=.*[@#!+^%$&*]).{8,14}", pass)){
+            return true;
+        }
+        System.out.println("1. Password must contain from 8 to 14 characters.\n" +
+                "2. Each password must contain upper and lower case characters.\n" +
+                "3. Each password must contain numbers.\n" +
+                "4. Each password must contain special characters into it.");
+        return false;
+    }
+    public String forgetPassword(){
+        System.out.print("Please enter new password again: ");
+        String m = in.next();
+        while(true) {
+            if (!checkPasswordGuidelines(m)) {
+                System.out.print("Please enter another password: ");
+                m = in.next();
+            }else{
+                return m;
+            }
+        }
+    }
+
+
+
+
 }
